3211fe3bd14954912c73177c91e1a6625246c775
SIddhant Pawar <165921076+Siddhant4912@users.noreply.github.com>
2025-07-28 22:39:27 +0530

    Update settings.py

--- QuickReserve/QuickReserve/settings.py
+++ QuickReserve/QuickReserve/settings.py
@@ -45,8 +45,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'syitsd20if002@gmail.com'  # Your email
-EMAIL_HOST_PASSWORD = 'tywkgkcscogpjydu' 
+EMAIL_HOST_USER = 'sendermail@gmail.com'  # Your email
+EMAIL_HOST_PASSWORD = 'thhsjlkdjdsfjkldfj' 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
